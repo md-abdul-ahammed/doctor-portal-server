a2df08842ef82585ba9545dba434bf13096569c4
md-abdul-ahammed <shagarmd2082@gmail.com>
2021-11-10 00:44:52 +0600

    allDone

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 // doctors-portal-firebase-adminsdk.json
 
 
-const serviceAccount = require("./doctors-portal-firebase-adminsdk.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
